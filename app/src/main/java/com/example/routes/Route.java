9192a68864d22bc37a2b1abc851dd2656d077c81
paep3nguin <paep3nguin@gmail.com>
2017-02-23 22:26:05 -0600

    Move Route and Stop to own files

---
+++ app/src/main/java/com/example/routes/Route.java
@@ -0,0 +1,17 @@
+package com.example.routes;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+/**
+ * Represents a bus route
+ */
+public class Route {
+    int id;
+    String name;
+    String short_name;
+    int color;
+    List<LatLng> path;
+    List<Integer> stops;
+}
